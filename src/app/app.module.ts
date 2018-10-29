4c4ac020982ba68fd035b63dfeda5d2c7046511a
Miguel Ángel Ortiz BErmejo <miguelaob@gmail.com>
2018-10-29 21:47:48 +0100

    Changes in modedel and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+import { PharmacyPage } from '../pages/pharmacy/pharmacy.component';
+import { AddPharmacyPage } from '../pages/pharmacy/add/add.pharmacy.component';
+
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { PharmacyService } from '../services/pharmacy.service';
 
 @NgModule({
   declarations: [
+    PharmacyPage,
+    AddPharmacyPage,
     MyApp,
     AboutPage,
-    ContactPage,
-    HomePage,
     TabsPage
   ],
   imports: [
@@ -25,13 +27,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   bootstrap: [IonicApp],
   entryComponents: [
+    PharmacyPage,
+    AddPharmacyPage,
     MyApp,
     AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
   ],
   providers: [
+    PharmacyService,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
