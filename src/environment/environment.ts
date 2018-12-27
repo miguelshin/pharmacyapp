8534302ffe5097547eea71fc147a4e516dd315db
Miguel Ángel Ortiz BErmejo <miguelaob@gmail.com>
2018-12-27 03:43:08 +0100

    Cordova changes

---
+++ src/environment/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBLlVgH2gmsYWyi5CR9mzQhv9scp1b6XPM",
+        authDomain: "pharmacyapp-b56e1.firebaseapp.com",
+        databaseURL: "https://pharmacyapp-b56e1.firebaseio.com",
+        projectId: "pharmacyapp-b56e1",
+        storageBucket: "",
+        messagingSenderId: "86029123794"
+    }
+};
